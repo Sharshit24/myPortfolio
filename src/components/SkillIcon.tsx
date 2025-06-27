551a9d95152ad687bc93d8b6ac46d48015150b43
Sumit Verma <139337175+Sharshit24@users.noreply.github.com>
2025-06-27 17:47:52 +0530

    skills add on

--- src/components/SkillIcon.tsx
+++ src/components/SkillIcon.tsx
@@ -28,6 +28,7 @@ export const SkillIcon: React.FC<SkillIconProps> = ({ name, className = '' }) =>
     'GitHub': SiGithub,
     'Python': SiPython,
     'C++': SiCplusplus,
+    'Golang': SiGOLANG
   };
 
   const Icon = iconMap[name];
