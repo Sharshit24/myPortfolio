76bed7eed743f8f55c191cc3787eeb0275e3e75e
Sumit <vermasumitharshit@gmail.com>
2026-01-30 16:11:38 +0530

    updationnn

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const root = process.cwd();
+
+export default {
   turbopack: {
     // Ensure Turbopack picks the correct workspace root when multiple lockfiles exist
-    root: __dirname,
+    root,
   },
 };
